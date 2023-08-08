a16b2c99df969f3269efed43f8799f86253e783c
agaria-rep <96198356+agaria-rep@users.noreply.github.com>
2023-08-08 15:31:53 +0200

    time

--- static/js/time.js
+++ static/js/time.js
@@ -5,20 +5,34 @@ function newTime() {
     let now = new Date();
     
     times.forEach(function(e) {
-        let timeH = now.getUTCHours();
-        let timeM = now.getUTCMinutes();
-        let timeS = now.getUTCSeconds();
+        let time = new Date(now.toUTCString());
 
-        timeH += parseInt(e.getAttribute("utc"));
+        time.setUTCHours(time.getUTCHours()+parseInt(e.getAttribute("utc")));
 
-        timeH = timeH.toString();
-        timeM = timeM.toString();
-        timeS = timeS.toString();
-    
-        if (timeH.length < 2) timeH = "0" + timeH;
-        if (timeM.length < 2) timeM = "0" + timeM;
-        if (timeS.length < 2) timeS = "0" + timeS;
-    
-        e.querySelector(".time-time").innerHTML = timeH + ":" + timeM + ":" + timeS;
+        let output = "<span class=\"time-date-span\">";
+
+        let output_date = parseInt(time.getUTCDate());
+        if (output_date.toString().length < 2) output += "0"; 
+        output += output_date.toString() + ".";
+
+        let output_mounth = parseInt(time.getUTCMonth())+1;
+        if (output_mounth.toString().length < 2) output += "0"; 
+        output += output_mounth.toString() + ".";
+
+        output += time.getUTCFullYear().toString() + "</span> ";
+
+        let output_hour = parseInt(time.getUTCHours());
+        if (output_hour.toString().length < 2) output += "0"; 
+        output += output_hour.toString() + ":";
+
+        let output_minute = parseInt(time.getUTCMinutes());
+        if (output_minute.toString().length < 2) output += "0"; 
+        output += output_minute.toString() + ":";
+
+        let output_second = parseInt(time.getUTCSeconds());
+        if (output_second.toString().length < 2) output += "0"; 
+        output += output_second.toString();
+
+        e.querySelector(".time-time").innerHTML = output;
     });
 }
